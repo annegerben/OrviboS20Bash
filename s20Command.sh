3630333022a0e83b6e41585b32a5e9d5df8d2b48
annegerben <annegerben@users.noreply.github.com>
2017-04-05 16:46:00 +0200

    added poll and correct function when manual swichting is used

--- s20Command.sh
+++ s20Command.sh
@@ -2,6 +2,7 @@
 action=$1
 mac=$2
 ip=$3
+idx=$4
 port="10000"
 twenties="202020202020"
 reversedMac=${mac:10:1}${mac:11:1}${mac:8:1}${mac:9:1}${mac:6:1}${mac:7:1}${mac:4:1}${mac:5:1}${mac:2:1}${mac:3:1}${mac:0:1}${mac:1:1}
@@ -27,6 +28,22 @@ get)
 		echo "switch is on";
 	else
 		echo "ERROR";
+	fi
+poll)
+        # get status of vswitch
+	vstatus = curl -s 'http://127.0.0.1:8080/json.htm?type=devices&rid='$idx | jq -r '.result[].Data'
+	if [ $vstatus = 'Off' ]; then
+		vstatus = 0;
+	elif if [ $vstatus = 'On' ]; then
+		vstatus = 1;
+	else
+		echo "ERROR";
+	fi
+	# Switch status
+	if [ ${status:47} = $vstatus ]; then
+		echo "ok";
+	else
+		curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=switchlight&idx='$idx'&switchcmd=Toggle';
 	fi
 esac
 
